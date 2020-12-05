6c7afa468f976aa3b111933d665603675cda4f5a
Rui Ventura <rventura.pt@outlook.com>
2020-12-05 21:03:57 +0000

    day 3: avoid shadowing existing identifiers

--- 03/run.jl
+++ 03/run.jl
@@ -1,19 +1,19 @@
 #!/usr/bin/env julia
 
-const map = readlines(joinpath(@__DIR__, "input.txt"))
+const forest = readlines(joinpath(@__DIR__, "input.txt"))
 
-treecount(map, dx = 3, dy = 1) =
-    let w = length(map[1]),
-        h = length(map)
+treecount(forest, dx = 3, dy = 1) =
+    let w = length(forest[1]),
+        h = length(forest)
         count(enumerate(dy + 1:dy:h)) do (i, j)
-            map[j][mod1(dx * i + 1, w)] == '#'
+            forest[j][mod1(dx * i + 1, w)] == '#'
         end
     end
 
 # part 1
-println(treecount(map))
+println(treecount(forest))
 
 # part 2
-treecount.([map],
+treecount.([forest],
            [1 3 5 7 1],
            [1 1 1 1 2]) |> prod |> println
