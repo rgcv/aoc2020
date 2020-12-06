7188f64c3f9c89c6a495b926f55dae52f2166fb2
Rui Ventura <rventura.pt@outlook.com>
2020-12-06 20:51:48 +0000

    day 6: `mapreduce` means less parenthesis

--- 06/run.jl
+++ 06/run.jl
@@ -4,7 +4,7 @@ const input_path = joinpath(@__DIR__, "input.txt")
 const groups = split.(split(read(input_path, String), "\n\n"))
 
 # part 1
-println(sum(map(length ∘ unique ∘ join, groups)))
+println(mapreduce(length ∘ unique ∘ join, +, groups))
 
 # part 2
-println(sum(map(length, intersect(g...) for g ∈ groups)))
+println(mapreduce(length, +, intersect(g...) for g ∈ groups))
