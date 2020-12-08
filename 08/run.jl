5703c13cc79b6aab9537e17879e2c93ff22ba2f9
Rui Ventura <rventura.pt@outlook.com>
2020-12-08 19:14:47 +0000

    day 8: make `Program` constructors more open+robust

--- 08/run.jl
+++ 08/run.jl
@@ -5,23 +5,35 @@ mutable struct Program
     pc::Int
     code::Array{String}
     visited::Array{Bool}
-
-    Program(code::AbstractVector{<:AbstractString}) =
-        new(0, 1, copy(string.(code)), zeros(Bool, length(code)))
+    function Program(acc::Int,
+                     pc::Int,
+                     code::AbstractVector{<:AbstractString},
+                     visited::AbstractVector{Bool})
+        @boundscheck let len = length(code)
+            1 ≤ pc ≤ len + 1 ||
+            throw(ErrorException("program counter goes beyond program end"))
+            length(visited) == len ||
+            throw(ErrorException("`visited` vector must be the same length as the code (=$len)"))
+        end
+        new(acc, pc, string.(code), copy(visited))
+    end
 end
+Program(code::AbstractVector{<:AbstractString}) =
+    @inbounds Program(0, 1, string.(code), falses(length(code)))
 
 const program = Program(readlines(joinpath(@__DIR__, "input.txt")))
 
 getacc(p::Program) = p.acc
 islooping(p::Program) = p.visited[p.pc]
 terminated(p::Program) = p.pc > size(p)
+reset!(p::Program) = (p.acc = 0; p.pc = 1; p.visited .= false; p)
 
-Base.copy(p::Program) = Program(copy(p.code))
-Base.reset(p::Program) = Program(p.code)
+Base.copy(p::Program) = Program(p.acc, p.pc, copy(p.code), copy(p.visited))
 Base.getindex(p::Program, i::Integer) = p.code[i]
-Base.setindex!(p::Program, v, i::Integer) = p.code[i] = v
 Base.length(p::Program) = length(p.code)
+Base.setindex!(p::Program, v, i::Integer) = p.code[i] = v
 Base.size(p::Program) = length(p)
+Base.reset(p::Program) = reset!(copy(p))
 
 run(p::Program) = run!(copy(p))
 run!(p::Program) =
@@ -41,7 +53,7 @@ fix!(p::Program) =
     let oldcode = copy(p.code)
         run!(p)
         for i ∈ 1:size(p)
-            p = reset(p)
+            reset!(p)
             op, _ = split(p[i])
             newop = op == "jmp" ? "nop" :
                     op == "nop" ? "jmp" :
