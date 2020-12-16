5b0d1fa321864ee1f13ea2d1d35de36e9222ea90
Rui Ventura <rventura.pt@outlook.com>
2020-12-16 14:42:16 +0000

    day 15: refactoring, clarify `memorygame` is destructive

--- 15/run.jl
+++ 15/run.jl
@@ -1,19 +1,17 @@
 #!/usr/bin/env julia
 
-memorygame(ns, target = 2020) =
-    let lastseen = IdDict{Int,Int}(),
+memorygame!(ns, target = 2020) =
+    let cache = IdDict{Int,Int}(),
         len = length(ns),
         cur = ns[end]
 
-        foreach(enumerate(ns[1:end-1])) do (i, n)
-            lastseen[n] = i
-        end
         resize!(ns, target)
+        for (i, n) ∈ enumerate(ns[1:len - 1]) cache[n] = i end
 
         for turn ∈ len:target - 1
-            i = get!(lastseen, cur, turn)
-            lastseen[cur] = turn
-            cur = turn - i 
+            last = get!(cache, cur, turn)
+            cache[cur] = turn
+            cur = turn - last 
             ns[turn + 1] = cur
         end
 
@@ -22,6 +20,6 @@ memorygame(ns, target = 2020) =
 
 if abspath(PROGRAM_FILE) == @__FILE__
     input = [0, 13, 16, 17, 1, 10, 6]
-    println(memorygame(input))
-    println(memorygame(input, 30000000))
+    println(memorygame!(input))
+    println(memorygame!(input, 30000000))
 end
