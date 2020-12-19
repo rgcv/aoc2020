ca7c097f8b36b2e8362a324967a2312153754188
Rui Ventura <rventura.pt@outlook.com>
2020-12-19 10:04:28 +0000

    day 17: generalize `neighbors` for arbitrary dimensions

--- 17/run.jl
+++ 17/run.jl
@@ -1,21 +1,15 @@
 #!/usr/bin/env julia
 
+import Base.Iterators: product
+
 parseinput(filename) =
     let mgrid = map(line->map(==('#'), collect(line)), eachline(filename))
         rows, cols = length(mgrid), length(mgrid[1])
         IdDict((j, i, 0) => mgrid[i][j] for i ∈ 1:rows for j ∈ 1:cols)
     end
 
-neighbors(grid, t::NTuple{3,Int}) where N = neighbors3d(grid, t...)
-neighbors(grid, t::NTuple{4,Int}) where N = neighbors4d(grid, t...)
-neighbors3d(grid, x, y, z) =
-    [(i, j, k) => get(grid, (i, j, k), false)
-     for i ∈ x-1:x+1, j ∈ y-1:y+1, k ∈ z-1:z+1
-     if !(i == x && j == y && k == z)]
-neighbors4d(grid, x, y, z, w) =
-    [(i, j, k, l) => get(grid, (i, j, k, l), false)
-     for i ∈ x-1:x+1, j ∈ y-1:y+1, k ∈ z-1:z+1, l ∈ w-1:w+1
-     if !(i == x && j == y && k == z && l == w)]
+neighbors(grid::AbstractDict{T,<:Any}, t::T) where {N,T<:NTuple{N,Int}} =
+    (k => get(grid, k, false) for k ∈ product(map(v->v-1:v+1, t)...) if k ≠ t)
 
 augment(grid, x = 0) = IdDict((k..., x) => v for (k, v) ∈ grid)
 update(grid) =
